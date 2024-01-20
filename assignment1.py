5a13de5cc826d14b1bae6d4c8e86e8b2f80933b5
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-01-20 16:35:50 -0500

    test

---
+++ assignment1.py
@@ -0,0 +1,61 @@
+import string
+'''
+Melvin Berkoh
+CS 100 Section 002
+HW 01, January 19, 2024
+'''
+# three assignment lines, each of which creates a variable (object) with a meaningful name and assigns it an int value.
+
+currentAge = 20
+currentYear = 2024
+currentMonth = 1
+
+# three assignment lines, each of which creates a variable (object) with a meaningful name and assigns it a float value.
+
+money = 123.45
+weightInPounds = 123.23
+lengthInCm = 12.4
+
+# three assignment lines, each of which creates a string variable with a meaningful name.
+
+dateOfBirth = '2003-15-03'
+name = 'Melvin Berkoh'
+favoriteAnimal = 'Cat'
+
+'''
+Exercise 1.1
+
+1.In a print statement, what happens if you leave out one of the parentheses, or both?
+Answer: you will receive an error when trying to run the code. 
+2. If you are trying to print a string, what happens if you leave out one of the quotation marks,
+or both?
+Answer:  you will receive an error when trying to run the code
+3. You can use a minus sign to make a negative number like -2. What happens if you put a plus
+sign before a number? What about 2++2?
+8 Chapter
+'''
+'''
+Exercise 1.2. Start the Python interpreter and use it as a calculator.
+1. How many seconds are there in 42 minutes 42 seconds?
+2. How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+3. If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per
+mile in minutes and seconds)? What is your average speed in miles per hour?
+'''
+'''
+Exercise 2.1. Repeating my advice from the previous chapter, whenever you learn a new feature,
+you should try it out in interactive mode and make errors on purpose to see what goes wrong. • We’ve seen that n = 42 is legal. What about 42 = n?
+How about x = y = 1?
+• In some languages every statement ends with a semi-colon, ;. What happens if you put a
+semi-colon at the end of a Python statement?
+• What if you put a period at the end of a statement?
+• In math notation you can multiply x and y like this: xy. What happens if you try that in Python?
+
+'''
+
+'''
+Exercise 2.2. Practice using the Python interpreter as a calculator:
+1. The volume of a sphere with radius r is 43 πr3. What is the volume of a sphere with radius 5?
+2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
+3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+
+'''
