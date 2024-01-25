19055963d733e4060eef480ac337eceedd7080d3
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-01-25 11:06:15 -0500

    added another class example

---
+++ exercise2.py
@@ -0,0 +1,25 @@
+str = 'abcdefgh'
+# for a
+str[0]
+str[-8]
+# for b
+str[1]
+str[-7]
+# for c
+str[2]
+str[-6]
+# for d
+str[3]
+str[-5]
+# for e
+str[4]
+str[-4]
+# for f
+str[5]
+str[-3]
+# for g
+str[6]
+str[-2]
+# for h
+str[-1]
+str[7]
