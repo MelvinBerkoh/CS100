a6337941e0fd99e3cd9f7270cd78549bf7b0d964
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-01-25 10:59:22 -0500

    added some of the classwork examples we did pretty easy

--- classwork.py
+++ classwork.py
@@ -1,1 +1,19 @@
-print(17 % 5)
+s1 = 'good'
+s2 = 'bad'
+s3 = 'silly'
+
+# ll appers in s3
+print('ll' in s3)
+# blank space doesnt not appear in s1
+print(' ' in s1)
+# the concatenation of s1, s2 and s3
+s4 = s1+s2+s3
+print(s4)
+#  blank space appears in the concatenation of s1,s2,s3
+print(' ' in s4)
+#  the concatention of 10 copies of s3
+s5 = s3*10
+
+# the total number of characters in the concatenation of s1, s2, and s3
+
+print(len(s4))
