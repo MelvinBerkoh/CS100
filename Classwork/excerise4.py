d1ac1a64f00c9cc84216917508333a4edc7e313f
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-05 10:50:46 -0500

    couldnt get the turtle library to open screen

---
+++ Classwork/excerise4.py
@@ -0,0 +1,10 @@
+import turtle
+
+# today we are learning about turtle and creating graphics for gui
+# always need to add a screen
+myScreen = turtle.Screen()
+
+# this is the pen
+myPen = turtle.Turtle()
+
+myPen.forward(100)
