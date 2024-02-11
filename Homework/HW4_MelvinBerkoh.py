6e031acc8916c69dcd030b31d756162ed9c0ce19
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-11 09:08:24 -0500

    formated this file

--- Homework/HW4_MelvinBerkoh.py
+++ Homework/HW4_MelvinBerkoh.py
@@ -12,8 +12,12 @@ b. Write an if statement that prints “OK” if a is less than b
 c. Write an if statement that prints “OK” if c is less than b
 d. Write an if statement that prints “OK” if the sum of a and b is equal to c
 e. Write an if statement that prints “OK” if the sum of a squared and b squared equals c squared.
+'''
+'''
 2. Repeat the previous problem with the additional requirement that “NOT OK” is printed if the
 condition is false.
+'''
+'''
 3. Write a program that asks the user for a color, a line width, a line length and a shape. Assume that
 the user will specify a shape that is either a line, a triangle, or a square. Use turtle graphics to draw
 the shape that the user requests of the size, color, line width and line length that the user requests.
@@ -23,5 +27,4 @@ what color? blue
 what line width? 25
 what line length? 100
 line, triangle or square? triangle
-
 '''
