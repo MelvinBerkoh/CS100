1dfa9b6392e6ae66460c2525e095eda05b7a3800
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-12 10:27:11 -0500

    answered question 2

--- Homework/HW4_MelvinBerkoh.py
+++ Homework/HW4_MelvinBerkoh.py
@@ -32,16 +32,24 @@ condition is false.
 '''
 if a < b:
     print("OK")
+else:
+    print("NOT OK")
 #  Write an if statement that prints “OK” if c is less than b
 if c < b:
     print("OK")
+else:
+    print("NOT OK")
 #  Write an if statement that prints “OK” if the sum of a and b is equal to c
 if (a+b) == c:
     print("OK")
+else:
+    print("NOT OK")
 #  Write an if statement that prints “OK” if the sum of a squared and b squared equals c squared.
 
 if (a**2)+(b**2) == c**2:
     print("OK")
+else:
+    print("NOT OK")
 '''
 3. Write a program that asks the user for a color, a line width, a line length and a shape. Assume that
 the user will specify a shape that is either a line, a triangle, or a square. Use turtle graphics to draw
