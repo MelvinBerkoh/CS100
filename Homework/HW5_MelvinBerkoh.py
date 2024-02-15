7c42d46ef650346e36a764bfcd0f4c38bd809f53
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-15 10:13:47 -0500

    finished question 1

--- Homework/HW5_MelvinBerkoh.py
+++ Homework/HW5_MelvinBerkoh.py
@@ -4,10 +4,8 @@ CS 100 Section 002
 HW 05, February 15, 2024
 '''
 '''
-Create a list named
-months of the months of the year. Write a for loop that iterates over the elements
-of
-months and prints out each one that begins with letter 'J', one month per line.
+Create a list named months of the months of the year. Write a for loop that iterates over the elements
+of months and prints out each one that begins with letter 'J', one month per line.
 2. Write a for loop that iterates over all of the integers in the range 0 through 99, inclusive, and prints
 out all of those numbers that are divisible by both 2 and 5.
 3. Consider the strings created by these assignment statements:
@@ -19,3 +17,8 @@ horton.
 
 
 '''
+months = ['January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+for month in months:
+    if month[0].lower() == 'j':
+        print(month)
