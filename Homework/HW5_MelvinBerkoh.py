5a64645f4577082f4263d0de51ab52d2b89420ff
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-15 10:31:56 -0500

    added comments to code

--- Homework/HW5_MelvinBerkoh.py
+++ Homework/HW5_MelvinBerkoh.py
@@ -3,23 +3,14 @@ Melvin Berkoh
 CS 100 Section 002
 HW 05, February 15, 2024
 '''
-'''
-Create a list named months of the months of the year. Write a for loop that iterates over the elements
-of months and prints out each one that begins with letter 'J', one month per line.
 
-3. Consider the strings created by these assignment statements:
-horton = "A person's a person, no matter how small."
-vowels = "aeiouAEIOU"
-Write a for loop that prints out all the vowels in
-horton in the order they appear in
-horton.
-
-
-'''
+# Create a list named months of the months of the year.
 months = ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December']
 for month in months:
+  # Write a for loop that iterates over the elements of months
     if month[0].lower() == 'j':
+      # prints out each one that begins with letter 'J', one month per line.
         print(month)
 
 # 2. Write a for loop that iterates over all of the integers in the range 0 through 99, inclusive, and prints
