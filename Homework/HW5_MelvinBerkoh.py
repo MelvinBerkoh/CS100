1a12eee36714c29009cadf1e211a3ef413407550
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-15 10:07:05 -0500

    intial file added

---
+++ Homework/HW5_MelvinBerkoh.py
@@ -0,0 +1,21 @@
+'''
+Melvin Berkoh
+CS 100 Section 002
+HW 05, February 15, 2024
+'''
+'''
+Create a list named
+months of the months of the year. Write a for loop that iterates over the elements
+of
+months and prints out each one that begins with letter 'J', one month per line.
+2. Write a for loop that iterates over all of the integers in the range 0 through 99, inclusive, and prints
+out all of those numbers that are divisible by both 2 and 5.
+3. Consider the strings created by these assignment statements:
+horton = "A person's a person, no matter how small."
+vowels = "aeiouAEIOU"
+Write a for loop that prints out all the vowels in
+horton in the order they appear in
+horton.
+
+
+'''
