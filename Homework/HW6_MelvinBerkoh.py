1164c29e568ed48b47d7b9a90ae53a64158fdf5f
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-17 20:12:25 -0500

    added intial homework file

---
+++ Homework/HW6_MelvinBerkoh.py
@@ -0,0 +1,20 @@
+'''
+Melvin Berkoh
+CS 100 Section 002
+HW 06, February 17, 2024
+'''
+''''
+a. Write a function named isDivisible that takes two parameters
+1.maxInt, an integer
+2.twoInts, a tuple of two integers
+The function isDivisible should create and return a list of all the ints in the range from 1 to
+maxInt (not including maxInt) that are divisible of both ints in
+twoInts.
+b. Create three test cases, each consisting of a value for
+maxInt and a value for
+twoInts, for your
+function in Problem 2a. One of these tests should return the empty list. For each test case
+
+'''
+def isDivisible(maxInt,twoInts):
+  
