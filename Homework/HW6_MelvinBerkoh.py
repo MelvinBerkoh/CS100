d45047f0d18b7d7c72aae7a0cd3ba561f4e205d2
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-17 23:05:29 -0500

    code is done just have to go back and comment

--- Homework/HW6_MelvinBerkoh.py
+++ Homework/HW6_MelvinBerkoh.py
@@ -4,17 +4,37 @@ CS 100 Section 002
 HW 06, February 17, 2024
 '''
 ''''
-a. Write a function named isDivisible that takes two parameters
-1.maxInt, an integer
-2.twoInts, a tuple of two integers
-The function isDivisible should create and return a list of all the ints in the range from 1 to
-maxInt (not including maxInt) that are divisible of both ints in twoInts.
-b. Create three test cases, each consisting of a value for
-maxInt and a value for
-twoInts, for your
-function in Problem 2a. One of these tests should return the empty list. For each test case
+Write a function named
+hasFinalLetter that takes two parameters
+1.
+strList, a list of non-empty strings
+2.
+letters, a string of upper and/or lower case letters
+The function hasFinalLetter should create and return a list of all the strings in strList that end with a letter in letters.
+b. Create three test cases, each consisting of a list of non-empty strings and a string of upper
+and/or lower case letters, for your function in Problem 1a. One of these tests should return
+the empty list. For each test case write two assignment statements and a function call that
+pass the test arguments to your function.
 
 '''
+
+
+def hasFinalLetter(strList, letters):
+    ''' takes a list of strings and letter to match at the last index of each word in the string list'''
+    lstOfStrings = []
+    for word in strList:
+        if (word[len(word)-1]) == letters:
+            lstOfStrings.append(word)
+
+    return lstOfStrings
+
+
+# Test Cases
+print(hasFinalLetter(['Melvin', 'Berkoh', 'is', 'a', 'student'], 'n'))
+print(hasFinalLetter(['Melvin', 'Berkoh', 'is', 'a', 'student'], 'o'))
+print(hasFinalLetter(['Melvin', 'Berkoh', 'is', 'a', 'student'], 't'))
+
+
 # Write a function named isDivisible that takes two parameters 1.maxInt, an integer 2.twoInts, a tuple of two integers
 
 
