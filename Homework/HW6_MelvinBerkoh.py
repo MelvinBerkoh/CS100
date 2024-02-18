a9290e92e354d3effc7311bcbe58b3865da18429
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-17 22:09:13 -0500

    verison 1

--- Homework/HW6_MelvinBerkoh.py
+++ Homework/HW6_MelvinBerkoh.py
@@ -8,13 +8,27 @@ a. Write a function named isDivisible that takes two parameters
 1.maxInt, an integer
 2.twoInts, a tuple of two integers
 The function isDivisible should create and return a list of all the ints in the range from 1 to
-maxInt (not including maxInt) that are divisible of both ints in
-twoInts.
+maxInt (not including maxInt) that are divisible of both ints in twoInts.
 b. Create three test cases, each consisting of a value for
 maxInt and a value for
 twoInts, for your
 function in Problem 2a. One of these tests should return the empty list. For each test case
 
 '''
-def isDivisible(maxInt,twoInts):
-  
+# Write a function named isDivisible that takes two parameters 1.maxInt, an integer 2.twoInts, a tuple of two integers
+
+
+def isDivisible(maxInt, twoInts):
+    #   create and return a list of all the ints in the range from 1 to
+    # maxInt (not including maxInt) that are divisible of both ints in twoInts.
+    lstOfInts = []
+    for number in range(maxInt):
+        if (number % (twoInts[0]*twoInts[1]) == 0):
+            lstOfInts.append(number)
+
+    return lstOfInts
+
+
+print(isDivisible(28, (2, 4)))
+print(isDivisible(100, (5, 10)))
+print(isDivisible(32, (4, 8)))
