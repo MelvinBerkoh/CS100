4e7105f2e5f30a597d71dd623a371ecf13db8d1d
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-02-20 20:46:05 -0500

    finsihed final copy to submit

--- Homework/HW6_MelvinBerkoh.py
+++ Homework/HW6_MelvinBerkoh.py
@@ -9,7 +9,7 @@ def hasFinalLetter(strList, letters):
     ''' takes a list of strings and letter to match at the last index of each word in the string list'''
     lstOfStrings = []
     for word in strList:
-        if (word[len(word)-1]) == letters:
+        if (word[len(word)-1]) in letters:
             lstOfStrings.append(word)
 
     return lstOfStrings
@@ -17,13 +17,13 @@ def hasFinalLetter(strList, letters):
 
 # Test Cases
 testCase1 = ['Melvin', 'Berkoh', 'is', 'a', 'student']
-testCase1Letter = 'n'
+testCase1Letter = 'nhjl'
 print(hasFinalLetter(testCase1, testCase1Letter))
 testCase2 = ['Melvin', 'Berkoh', 'is', 'a', 'student']
-testCase2Letter = 'o'
+testCase2Letter = 'oxZ'
 print(hasFinalLetter(testCase2, testCase2Letter))
 testCase3 = ['Melvin', 'Berkoh', 'is', 'a', 'student', 'bucket']
-testCase3Letter = 't'
+testCase3Letter = 'tAbx'
 print(hasFinalLetter(testCase3, testCase3Letter))
 
 
@@ -31,7 +31,7 @@ print(hasFinalLetter(testCase3, testCase3Letter))
 def isDivisible(maxInt, twoInts):
     # create and return a list of all the ints in the range from 1 to maxInt (not including maxInt) that are divisible of both ints in twoInts.
     lstOfInts = []
-    for number in range(maxInt):
+    for number in range(1, maxInt):
         if (number % twoInts[0] == 0) and (number % twoInts[1] == 0):
             lstOfInts.append(number)
     return lstOfInts
@@ -43,7 +43,7 @@ maxInt2 = 100
 maxInt3 = 32
 t1 = (2, 4)
 t2 = (5, 10)
-t3 = (4, 8)
+t3 = (7, 8)
 print(isDivisible(maxInt1, t1))
 print(isDivisible(maxInt2, t2))
 print(isDivisible(maxInt3, t3))
