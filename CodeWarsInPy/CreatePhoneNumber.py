8901e1e010274728b12f0ca8ad60e929bbdf41b2
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-04 10:25:46 -0500

    finished final code

--- CodeWarsInPy/CreatePhoneNumber.py
+++ CodeWarsInPy/CreatePhoneNumber.py
@@ -1,6 +1,6 @@
 # Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 def create_phone_number(lst):
-    string = '{} {} {}'
+    string = '({}) {}-{}'
     temp = ''
     for num in lst:
         temp += str(num)
