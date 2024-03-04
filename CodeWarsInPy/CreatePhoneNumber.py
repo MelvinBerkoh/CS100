f2af484bc74cb22ae03048fc65534259dc10b72c
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-04 10:32:07 -0500

    added link to codewars kata

--- CodeWarsInPy/CreatePhoneNumber.py
+++ CodeWarsInPy/CreatePhoneNumber.py
@@ -1,3 +1,4 @@
+# Question link from code wars https://www.codewars.com/kata/525f50e3b73515a6db000b83/python
 # Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 def create_phone_number(lst):
     string = '({}) {}-{}'
