aa58cd402735ba602e6fd48d66073b9d227af92f
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-04 10:31:04 -0500

    added test cases

--- CodeWarsInPy/CreatePhoneNumber.py
+++ CodeWarsInPy/CreatePhoneNumber.py
@@ -8,4 +8,10 @@ def create_phone_number(lst):
     return string
 
 
+# test cases
 print(create_phone_number([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]))
+print(create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
