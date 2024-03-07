7604b59f7055e8b2f1deff0a81bb68cbe5b2717c
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-07 10:24:43 -0500

    added question

---
+++ Homework/HW8_MelvinBerkoh.py
@@ -0,0 +1,21 @@
+'''
+This problem provides practice using a while True loop.
+Write a function named twoWords that gets and returns two words from a user. The first word is of a
+specified length, and the second word begins with a specified letter.
+The function twoWords takes two parameters:
+1. an integer, length, that is the length of the first word and
+2. a character, firstLetter, that is the first letter of the second word. The second word may begin with
+either an upper or lower case instance of firstLetter.
+The function twoWords should return the two words in a list. Use a while True loop and a break statement
+in the implementation of twoWords.
+The following is an example of the execution of twoWords:
+print(twoWords(4, 'B'))
+Enter a 4-letter word please: two
+Enter a 4-letter word please: one
+Enter a 4-letter word please: four
+Enter a word beginning with B please: apple
+Enter a word beginning with B please: pear
+Enter a word beginning with B please: banana
+['four', 'banana']
+
+'''
