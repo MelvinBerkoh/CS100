e0e94ebb1707fd0a1fae3b7b330338769e58e54a
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-12 23:27:19 -0400

    major progress start to use dictionaries

--- CodeWarsInPy/SimpleFrequencySort.py
+++ CodeWarsInPy/SimpleFrequencySort.py
@@ -4,26 +4,15 @@
 # with that highest frequecny number we want to add that to the new list as the count of that number
 # reapeat for all elements in the list
 def highestFreq(arr):
-    highestCount = ''
-    counter = 0
-  #  list to hold the result
-    newList = []
-    # list so that we dont loop twice
-    accountForNumber = []
-    # loop through all the elments of the array/list
+    #  create dictionary to hold key as the number and count as the value
+    values = {}
+    accountedValues = []
     for num in arr:
-        if num not in accountForNumber:
-            accountForNumber.append(num)
-        # get the count of each element in list
+        if num not in accountedValues:
+            accountedValues.append(num)
             frequency = arr.count(num)
-            # determine the number with the highest count
-            for i in range(frequency):
-                # add that count to the new list
-                newList.append(num)
-            # sort list in decreasing order
-    newList.sort(reverse=True)
-
-    return newList
+            values[num] = frequency
+    return values
 
 
 print(highestFreq([2, 3, 5, 3, 7, 9, 5, 3, 7]))
