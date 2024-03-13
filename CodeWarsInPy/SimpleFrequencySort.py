60c021283b17017abc9aa21eab56bd97d13b1e72
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-12 22:19:48 -0400

    stuck

--- CodeWarsInPy/SimpleFrequencySort.py
+++ CodeWarsInPy/SimpleFrequencySort.py
@@ -1,15 +1,23 @@
 # In this kata, you will sort elements in an array by decreasing frequency of elements. If two elements have the same frequency, sort them by increasing value.
-
+# loop through arr and make sure its not in the created new list
+# as we are looping through the array we want to find the the number that appears the most
+# with that highest frequecny number we want to add that to the new list as the count of that number
+# reapeat for all elements in the list
 def highestFreq(arr):
     highestFrequency = ''
     counter = 0
     newList = []
     for num in arr:
-        if arr.count(num) > counter:
-            highestFrequency = num
-            counter = arr.count(num)
-        arr.remove(num)
-    newList.append(highestFrequency)
+    #     if num in newList:
+    #         continue
+    #     else:
+    #         if arr.count(num) > counter:
+    #             counter = arr.count(num)
+    #             highestFrequency += str(num)
+    # newList.append(highestFrequency)
+
+    #     arr.remove(num)
+
     return newList
 
 
