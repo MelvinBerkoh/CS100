624542967a4e92c90595893ea0516ca41102a0a8
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-28 10:26:14 -0400

    finished fucntion named intialLetter

--- Homework/HW10_MelvinBerkoh.py
+++ Homework/HW10_MelvinBerkoh.py
@@ -1,27 +1,7 @@
-'''
 
-Problem 2
-Write a function named
-initialLetters that takes one parameter,
-wordList – a list of words. Create and return
-a dictionary in which each initial letter of a word in
-wordList is a key and the corresponding value is a list
-of the words in
-wordList that begin with that letter. There should be no duplicate words in any value in the
-dictionary.
-For example, the following is correct output:
-print(initialLetters(horton))
-{'I': ['I'], 's': ['say'], 'w': ['what'], 'm': ['mean'], 'a': ['and']}
 
-'''
 '''
 Problem 1
- The keys in the dictionary should be case-
-sensitive, which means 'a' and 'A' are two different keys.
-For example, the following is correct output:
-horton = ['I', 'say', 'what', 'I', 'mean', 'and', 'I', 'mean', 'what', 'I', 'say']
-print(initialLetterCount(horton))
-{'I': 4, 's': 2, 'w': 2, 'm': 2, 'a': 1}
 '''
 
 
@@ -51,3 +31,42 @@ def initialLetterCount(wordList):
 # test case
 print(initialLetterCount(['I', 'say', 'what', 'I', 'mean', 'A',
                           'and', 'I', 'mean', 'what', 'I', 'say']))
+
+'''
+
+Problem 2
+Write a function named
+initialLetters that takes one parameter,
+wordList – a list of words. Create and return
+a dictionary in which each initial letter of a word in
+wordList is a key and the corresponding value is a list
+of the words in
+wordList that begin with that letter. There should be no duplicate words in any value in the
+dictionary.
+For example, the following is correct output:
+print(initialLetters(horton))
+{'I': ['I'], 's': ['say'], 'w': ['what'], 'm': ['mean'], 'a': ['and']}
+
+'''
+
+
+def initialLetters(wordList):
+    # create the dictionary
+    result = {}
+    # loop though the wordList
+    for word in wordList:
+        # check to see if the first letter in the word we are looking at is in the dictionary
+        if word[0] in result:
+
+            # if found add the word to the value of that key
+            result[word[0]] = word
+        else:
+            # if the first letter of the word  isn't found create a key with the letter and make the value word
+            result[word[0]] = word
+
+    # return the dictionary
+    return result
+
+
+print(initialLetters(['I', 'say', 'what', 'I', 'mean', 'A',
+                      'and', 'I', 'mean', 'what', 'I', 'say']))
