49b8d88a3941ddab8f1ac0e16eaa73f4c458a1ac
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-03-28 10:03:19 -0400

    finished function 1

--- Homework/HW10_MelvinBerkoh.py
+++ Homework/HW10_MelvinBerkoh.py
@@ -1,17 +1,5 @@
 '''
-Problem 1
-Write a function named
-initialLetterCount that takes one parameter,
-wordList — a list of words. Create and
-return a dictionary in which each initial letter of a word in
-wordList is a key and the corresponding value
-is the number of words in
-wordList that begin with that letter. The keys in the dictionary should be case-
-sensitive, which means 'a' and 'A' are two different keys.
-For example, the following is correct output:
-horton = ['I', 'say', 'what', 'I', 'mean', 'and', 'I', 'mean', 'what', 'I', 'say']
-print(initialLetterCount(horton))
-{'I': 4, 's': 2, 'w': 2, 'm': 2, 'a': 1}
+
 Problem 2
 Write a function named
 initialLetters that takes one parameter,
@@ -26,3 +14,37 @@ print(initialLetters(horton))
 {'I': ['I'], 's': ['say'], 'w': ['what'], 'm': ['mean'], 'a': ['and']}
 
 '''
+'''
+Problem 1
+Write a function named
+initialLetterCount that takes one parameter,
+wordList — a list of words. Create and
+return a dictionary in which each initial letter of a word in
+wordList is a key and the corresponding value
+is the number of words in
+wordList that begin with that letter. The keys in the dictionary should be case-
+sensitive, which means 'a' and 'A' are two different keys.
+For example, the following is correct output:
+horton = ['I', 'say', 'what', 'I', 'mean', 'and', 'I', 'mean', 'what', 'I', 'say']
+print(initialLetterCount(horton))
+{'I': 4, 's': 2, 'w': 2, 'm': 2, 'a': 1}
+'''
+
+
+def initialLetterCount(wordList):
+    # create the dictionary
+    result = {}
+
+    # loop though the wordList
+    for word in wordList:
+        if word[0] in result:
+            result[word[0]] += 1
+        else:
+            firstLetter = word[0]
+            result[firstLetter] = 1
+
+    return result
+
+
+print(initialLetterCount(['I', 'say', 'what', 'I', 'mean',
+                          'and', 'I', 'mean', 'what', 'I', 'say']))
