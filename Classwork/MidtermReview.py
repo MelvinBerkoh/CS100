10e5a8c07706bc80c5a1b62fbc061159a78364b2
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-03 22:33:55 -0400

    added header

--- Classwork/MidtermReview.py
+++ Classwork/MidtermReview.py
@@ -3,12 +3,21 @@ This is a testing file for midterm questions
 '''
 
 
-def myFunction(rows, cols):
-    result = 0
-    for i in range(rows):
-        for j in range(cols):
-            result += j
-    return result
+def myFunction(numList):
+    for row in numList:
+        counter = 0
+        for num in row:
+            counter += 1
+            if num == 0 and counter != 2:
+
+                break
+            print(num, end=' ')
+        print()
 
 
-print(myFunction(3, 4))
+table = [
+    [2, 3, 0, 6],
+    [0, 3, 4, 5],
+    [4, 5, 6, 0]]
+
+myFunction(table)
---
+++ Homework/HW11_MelvinBerkoh.py
@@ -0,0 +1,3 @@
+'''
+Melvin Berkoh
+'''
