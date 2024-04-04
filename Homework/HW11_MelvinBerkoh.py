68d79d39be0e0a4f9d3903993d37119d6c270734
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-04 10:17:14 -0400

    added problem 1

--- Homework/HW11_MelvinBerkoh.py
+++ Homework/HW11_MelvinBerkoh.py
@@ -1,3 +1,34 @@
 '''
 Melvin Berkoh
+CS 100 Section 002
+HW 11 April 11, 2024
+'''
+'''
+Problem 1
+Write a class definition line and a one line docstring for the class
+Dog. Write an
+__init__ method for the
+class
+Dog that gives each dog its own
+name and
+breed. Test this on a successful creation of a
+Dog object.
+>>> import dog
+>>> sugar = dog.Dog('Sugar', 'border collie')
+>>> sugar.name
+'Sugar'
+>>> sugar.breed
+'border collie'
+Problem 2
+Add a data attribute
+tricks of type
+list to each
+Dog instance and initialize it in
+__init__ to the empty list. The
+user does not have to supply a list of tricks when constructing a
+Dog instance. Make sure that you test this
+successfully.
+>>> sugar.tricks
+[]
+
 '''
