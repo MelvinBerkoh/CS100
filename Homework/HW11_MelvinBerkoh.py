5543a82983601d400d935311c0042f51aba16d16
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-05 21:28:07 -0400

    classes and objbects lol

--- Homework/HW11_MelvinBerkoh.py
+++ Homework/HW11_MelvinBerkoh.py
@@ -6,13 +6,8 @@ HW 11 April 11, 2024
 '''
 Problem 1
 Write a class definition line and a one line docstring for the class
-Dog. Write an
-__init__ method for the
-class
-Dog that gives each dog its own
-name and
-breed. Test this on a successful creation of a
-Dog object.
+Dog. Write an __init__ method for the class Dog that gives each dog its own
+name and breed. Test this on a successful creation of a Dog object.
 >>> import dog
 >>> sugar = dog.Dog('Sugar', 'border collie')
 >>> sugar.name
@@ -29,12 +24,10 @@ class Dog:
 
 # this is the constructor for a python class
 # self parameter
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, tricks):
         self.name = name
         self.breed = breed
-
-    def show(self):
-        print(self.name, self.breed)
+        self.tricks = list(tricks)
 
 
 '''
--- Homework/testforHW11.py
+++ Homework/testforHW11.py
@@ -1,3 +1,6 @@
-import dog
+import HW11_MelvinBerkoh
 
-pta = dog.Dog()
+sugar = HW11_MelvinBerkoh.Dog('Sugar', 'border collie')
+print(sugar.breed)
+print(sugar.name)
+print(sugar.tricks)
