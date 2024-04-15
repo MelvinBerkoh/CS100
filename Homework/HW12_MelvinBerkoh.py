a2186112a7a8f6005cacbb72f1622aef0ed15379
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-15 10:59:47 -0400

    funished creating function safeFloat

--- Homework/HW12_MelvinBerkoh.py
+++ Homework/HW12_MelvinBerkoh.py
@@ -1,30 +1,5 @@
 '''
-
-Problem 1 Recall that when the built-in function open() is called to open a file for reading, but it doesn’t exist, an
-exception is raised. However, if the file exists, a reference to the opened file object is returned.
-Write a function
-safeOpen() that takes one parameter,
-filename — a string giving the pathname of the file
-to be opened for reading. When
-safeOpen() is used to open a file, a reference to the opened file object
-should be returned if no exception is raised, just like for the
-open() function. If an exception is raised
-while trying to open the file,
-safeOpen() should return the value None.
-For example, assuming the file
-ghost.txt doesn’t exist, the following is correct output:
->>> # open()
->>> print(open('ghost.txt'))
-Traceback (most recent call last):
-File "<pyshell#8>", line 1, in <module>
-print(open('ghost.txt'))
-FileNotFoundError: [Errno 2] No such file or directory: 'ghost.txt'
->>>
->>> # safeOpen()
->>> inputFile = safeOpen('ghost.txt')
->>> print(inputFile)
-None
->>>
+Problem 1
 '''
 
 
@@ -38,3 +13,22 @@ def safeOpen(fileName):
 
 # test cases
 print(safeOpen('ghost.txt'))
+
+
+'''
+Problem 2
+'''
+
+
+def safeFloat(x):
+    '''  Function that takes a number or string and returns a floating point number constructed from a number or string. If the string doesn’t represent a valid floating point value, an exception is raised. If an exception is raised while trying to convert a number or string, safeFloat() should return 0.0 floating point value.'''
+    try:
+        return float(x)
+
+    except ValueError:
+
+        return 0.0
+
+
+# testcase
+print(safeFloat('abc'))
