1becc317b38d83793ab1be4afec35d0fec8bd850
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-19 22:58:46 -0400

    finial verison of power point

--- Classwork/university.py
+++ Classwork/university.py
@@ -12,12 +12,6 @@ class Course_Section:
         self.sectionNumber = sectionNumber
         self.enrolledStudents = []
 
-    ''' Write a method enroll as part of the class Course_Section. The method enroll should add a
-passed tuple parameter to enrolled_students only if the tuple does not exists already in the list. The first
-item in the tuple is the name of the student and the second item is the ucid of the students. The enroll
-method will print a message confirming the enrollment of the student if the tuple gets added to the list
-otherwise it will print a message that the students is already enrolled'''
-
     def enroll(self, student):
         if student not in self.enrolledStudents:
             self.enrolledStudents.append(student)
@@ -26,11 +20,6 @@ otherwise it will print a message that the students is already enrolled'''
         else:
             print(f"Student {student[0]} with UCID {student[1]} is already enrolled in {
                   self.courseName} section {self.sectionNumber}")
-
-    ''' Write a method drop as part of the class Course_Section. The method drop should check
-whether a passed tuple (student name, ucid) is in the enrolled_students list or not. If the tuple exists in
-the list then the method removes that tuple and prints that the student is successfully dropped otherwise
-prints that the student does not exist'''
 
     def drop(self, student):
         if student in self.enrolledStudents:
