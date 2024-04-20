87740c04759b4a63816c149d8d88b3a032644f93
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-19 23:02:00 -0400

    created the string docs for the student class

--- Classwork/university.py
+++ Classwork/university.py
@@ -31,31 +31,26 @@ class Course_Section:
                   self.courseName} section {self.sectionNumber}")
 
 
-'''
-Write a class definition line and a one line docstring for the class Student. Write an
-__init__ method for the class Student that gives each student its own name and ucid. Make sure that
-you test this on a successful creation of a student object.
-'''
-
-
 class Student:
-    ''' holds the student name and ucid for the object created'''
+       ''' Write a class definition line and a one line docstring for the class Student. Write an
+  __init__ method for the class Student that gives each student its own name and ucid. Make sure that
+  you test this on a successful creation of a student object '''
 
-    def __init__(self, name, ucid):
-        self.name = name
-        self.ucid = ucid
-        self.transcript = {}
+       def __init__(self, name, ucid):
+            self.name = name
+            self.ucid = ucid
+            self.transcript = {}
 
-    def grade_obtained(self, course, grade):
-        self.transcript[course] = grade
-        print(f"Student {self.name} with UCID {
-              self.ucid} has obtained a grade of {grade} in {course}")
+        def grade_obtained(self, course, grade):
+            self.transcript[course] = grade
+            print(f"Student {self.name} with UCID {
+                  self.ucid} has obtained a grade of {grade} in {course}")
 
-    def viewGrade(self, courseName):
-        if courseName in self.transcript:
-            print(f"{self.name}, your grade for {
-                  courseName} is {self.transcript[courseName]}")
-            return self.transcript[courseName]
-        else:
-            print(f"{self.name}, you have not taken {courseName}")
-            return None
+        def viewGrade(self, courseName):
+            if courseName in self.transcript:
+                print(f"{self.name}, your grade for {
+                      courseName} is {self.transcript[courseName]}")
+                return self.transcript[courseName]
+            else:
+                print(f"{self.name}, you have not taken {courseName}")
+                return None
