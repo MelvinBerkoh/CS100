c1d8496aa08007aac28ebdd5712a6b8286784f17
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-26 23:38:23 -0400

    updates on testing

--- Classwork/BackAccounttester.py
+++ Classwork/BackAccounttester.py
@@ -5,6 +5,10 @@ def main():
     # create the user instances for the Bank Account
     user1 = BankAccount.UserAccount(
         'Melvin', 'Berkoh', '123-11-1234', '1234567890', 'xyz@gmail.com')
+    # whenever initializing a class, you need to pass all of the  the required parameters
+    user2 = BankAccount.UserAccount(
+        'John', 'Doe', '123-11-1234', '1234567890',)
+    print(user2.getBankName())
     print(user1.getBankName())
     print(user1.bankName)
     print(user1.getBalance())
