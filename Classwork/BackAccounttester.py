fbf6905c9efb199720c3e058288b622b8bd0aedb
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-04-27 23:56:02 -0400

    an account instance is the same as a User account instance

--- Classwork/BackAccounttester.py
+++ Classwork/BackAccounttester.py
@@ -8,6 +8,8 @@ def main():
     # whenever initializing a class, you need to pass all of the  the required parameters
     user2 = BankAccount.UserAccount(
         'John', 'Doe', '123-11-1234', '1234567890',)
+    # create the account instances for the Bank Account
+    user3 = BankAccount.Account('Jane', 'Doe', '123-11-1234', '1234567890', )
     print(user2.getBankName())
     print(user1.getBankName())
     print(user1.bankName)
