293367ce5a99bca5f982c33072aef8c1e9fc1ae0
Melvin <106451111+MakaveliMelly@users.noreply.github.com>
2024-05-02 23:51:29 -0400

    update

--- Classwork/BackAccounttester.py
+++ Classwork/BackAccounttester.py
@@ -6,15 +6,19 @@ def main():
     user1 = BankAccount.UserAccount(
         'Melvin', 'Berkoh', '123-11-1234', '1234567890', 'xyz@gmail.com')
     # whenever initializing a class, you need to pass all of the  the required parameters
-    user2 = BankAccount.UserAccount(
-        'John', 'Doe', '123-11-1234', '1234567890',)
+    # user2 = BankAccount.UserAccount(
+    #     'John', 'Doe', '123-11-1234', '1234567890',)
     # create the account instances for the Bank Account
     # whenever initializing a class, you need to pass all of the  the required parameters
-    user3 = BankAccount.Account('Jane', 'Doe', '123-11-1234', '1234567890', )
-    print(user2.getBankName())
+    user3 = BankAccount.Account(
+        'Jane', 'Doe', '123-11-1234', '1234567890', 'dus@gmail.com')
+    # print(user2.getBankName())
     print(user1.getBankName())
     print(user1.bankName)
-    print(user1.getBalance())
+    # so when a user is created by the parent if the child has a method built it the parent class cannot access it?? tested below.
+    # print(user1.getBalance())
+    print(user3.getBalance())
+    print(user3.getName())
 
 
 main()
